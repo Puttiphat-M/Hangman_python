51ffa52cca0010b6b1d85457f16fb96054608ad3
Puttiphat U <90830272+Puttiphat-M@users.noreply.github.com>
2023-10-21 01:13:46 +0700

    link client with server

--- client/main.py
+++ client/main.py
@@ -1,19 +1,35 @@
 import argparse
 import socket
+import time
 
 def main(port):
     host = "192.168.68.54"
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((host, port))
-
-    print("Client side:")
+    status = False
+    while not status:
+        try:
+            client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            client.connect((host, port))
+            status = True
+        except:
+            print("Server is not up yet, trying again in 5 seconds")
+            time.sleep(5)
     while True:
         data = client.recv(1024).decode()
         if not data:
             break
         print(data, end="")
-        guess = input("Your next guess: ")
-        client.send(guess.encode())
+        
+        if "You won" in data:
+            break
+        elif "You lost" in data:
+            break
+        else:
+            guess = input("Your next Guess: ")
+            while len(guess) != 1 or not guess.isalpha():
+                guess = input("Please enter a single letter: ")
+            client.send(guess.encode())
+
+    client.close()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Hangman Server")
